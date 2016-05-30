7adef2cfac7022a78e95f07b4e6c44c18229d200
cesar <cmcuriqueo@udc.edu.ar>
2016-05-30 11:15:16 -0300

    cosas

--- index/validacion.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-
-    var x = document.forms["myForm"]["fname"].value;
-
-    if (x == null || x == "") {
-        alert("Ingrese un nombre");
-        return false;
-    }
-}
--- intranet/negocio/miJavaScript.js
+++ intranet/negocio/miJavaScript.js
@@ -2,3 +2,10 @@ function goBack() {
 	window.history.go(-1);
 }
 
+function guardado(){
+	alert("Guardado...");
+}
+
+function modificado() {
+	alert("Modificado...");
+}
