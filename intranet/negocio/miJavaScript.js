1a247c6b9356cf24869bb2ae41f74130b11650c1
Cesar Matias Curiqueo <cmcuriqueo@udc.edu.ar>
2016-06-02 13:04:39 -0300

    cambios ;P

--- intranet/negocio/miJavaScript.js
+++ intranet/negocio/miJavaScript.js
@@ -10,3 +10,33 @@ function modificado() {
 	alert("Modificado...");
 }
 
+function confirmar()
+{
+    if (confirm("Se perderan todos los nuevos datos\n¿desea continuar?")) {
+        goBack();
+    }
+}
+
+function validar_numero_positico(num){
+	var aviso = document.getElementById("avisoNumero");
+	aviso.innerHTML = ""
+	if (!isNaN(num)) {
+		aviso.innerHTML = "ingrese un numero";
+		return false;
+	} else {
+		if(parseInt(num) <= 0){
+			aviso.innerHTML = "ingrese un numero mayor a CERO"
+			return false;
+		}
+	}
+	return true;
+}
+
+
+ //   isNaN(15) devolverá falso
+ //  isNaN(15p) devolverá verdadero
+ //   isNaN(15 12) devolverá verdadero, hay un espacio en blanco
+ //   isNaN(3.141592) devolverá falso
+ //   isNaN(“23”) devolverá falso, puede ser convertido a un valor numérico
+
+
